c34d673282887b0a31385f946ed56e94485fc112
Mahmoud Mansour <mansour@gmail.com>
2025-08-05 21:08:21 +0300

    Complete 2nd Video - Part 02 Methods Overloading

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -2,9 +2,33 @@
 {
     internal class Program
     {
+        static int SumNumbers(int x, int y)
+        {
+            return x + y;
+        }
+        static int SumNumbers(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+        static double SumNumbers(double x, double y)
+        {
+            return x + y;
+        }
         static void Main(string[] args)
         {
             #region Polymorphism
+
+            #endregion
+
+            #region Methods Overloading
+
+            //double A = 10.2, B = 20.3, C = 30;
+
+            ////int Result = SumTwoNumbers(A, B);
+            ////int Result = SumThreeNumbers(A, B, C);
+            //double Result = SumNumbers(10, 20, 30);
+
+            //Console.WriteLine(Result);
 
             #endregion
         }
