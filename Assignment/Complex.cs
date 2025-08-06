bdebd638c587a60820b85a1939d36984966b7dac
Mahmoud Mansour <mansour@gmail.com>
2025-08-06 21:49:17 +0300

    Complete Part 01 - Q03

---
+++ Assignment/Complex.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Complex
+    {
+        #region Properties
+        public int Real { get; set; }
+        public int Imag { get; set; }
+        #endregion
+
+        #region Constructor
+        public Complex(int real, int imag)
+        {
+            Real = real;
+            Imag = imag;
+        }
+        #endregion
+
+        #region Overload Operator
+        public static Complex operator +(Complex a, Complex b)
+        {
+            return new Complex(a.Real+b.Real, a.Imag+b.Imag);
+        }
+        public static Complex operator -(Complex a, Complex b)
+        {
+            return new Complex(a.Real - b.Real, a.Imag - b.Imag);
+        }
+        #endregion
+    }
+}
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+            #region Part 01
+
             #region Q1) Write a class named Calculator that contains a method named Add. Overload the Add method to:
 
             //Calculator calc = new Calculator();
@@ -15,14 +17,30 @@
             #endregion
 
             #region Q2) Create a class named Rectangle with the following constructors:
-            Rectangle R1 = new Rectangle();
-            Rectangle R2 = new Rectangle(10, 20);
-            Rectangle R3 = new Rectangle(15);
+            //Rectangle R1 = new Rectangle();
+            //Rectangle R2 = new Rectangle(10, 20);
+            //Rectangle R3 = new Rectangle(15);
 
-            Console.WriteLine($"R1: {R1.Width}, {R1.Height}");
-            Console.WriteLine($"R2: {R2.Width}, {R2.Height}");
-            Console.WriteLine($"R3: {R3.Width}, {R3.Height}");
+            //Console.WriteLine($"R1: {R1.Width}, {R1.Height}");
+            //Console.WriteLine($"R2: {R2.Width}, {R2.Height}");
+            //Console.WriteLine($"R3: {R3.Width}, {R3.Height}");
             #endregion
+
+            #region Q3) Define a class Complex Number that represents a complex number with real and imaginary parts.
+            Complex C01 = new Complex(8, 9);
+            Complex C02 = new Complex(5, 7);
+
+            Complex Sum = C01 + C02;
+            Complex Sub = C01 - C02;
+
+            Console.WriteLine($"Sum of the Complex is {Sum.Real} + {Sum.Imag}i");
+            Console.WriteLine($"Sub of the Complex is {Sub.Real} - {Sub.Imag}i");
+            #endregion
+
+            #endregion
+
+
+
         }
     }
 }
