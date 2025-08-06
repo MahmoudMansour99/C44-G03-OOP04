1080deef7e0a1a82aac8c0edf2805a4adf3e3bb7
Mahmoud Mansour <mansour@gmail.com>
2025-08-06 21:29:58 +0300

    Complete Part 01 - Q01

---
+++ Assignment/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1) Write a class named Calculator that contains a method named Add. Overload the Add method to:
+
+            Calculator calc = new Calculator();
+
+            Console.WriteLine(calc.Add(5, 10));
+            Console.WriteLine(calc.Add(5, 10, 15));
+            Console.WriteLine(calc.Add(5.5, 9.5));
+
+            #endregion
+        }
+    }
+}
