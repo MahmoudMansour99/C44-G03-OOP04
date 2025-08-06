08d66cc53a296474e0fff1be6c6489e7bb7e92f9
Mahmoud Mansour <mansour@gmail.com>
2025-08-06 21:38:07 +0300

    Complete Part 01 - Q02

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -6,12 +6,22 @@
         {
             #region Q1) Write a class named Calculator that contains a method named Add. Overload the Add method to:
 
-            Calculator calc = new Calculator();
+            //Calculator calc = new Calculator();
 
-            Console.WriteLine(calc.Add(5, 10));
-            Console.WriteLine(calc.Add(5, 10, 15));
-            Console.WriteLine(calc.Add(5.5, 9.5));
+            //Console.WriteLine(calc.Add(5, 10));
+            //Console.WriteLine(calc.Add(5, 10, 15));
+            //Console.WriteLine(calc.Add(5.5, 9.5));
 
+            #endregion
+
+            #region Q2) Create a class named Rectangle with the following constructors:
+            Rectangle R1 = new Rectangle();
+            Rectangle R2 = new Rectangle(10, 20);
+            Rectangle R3 = new Rectangle(15);
+
+            Console.WriteLine($"R1: {R1.Width}, {R1.Height}");
+            Console.WriteLine($"R2: {R2.Width}, {R2.Height}");
+            Console.WriteLine($"R3: {R3.Width}, {R3.Height}");
             #endregion
         }
     }
